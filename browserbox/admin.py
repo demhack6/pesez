5422466bc0dcf2c4d296c2a9c41c5642225f876f
devsagul <dev.sagul@gmail.com>
2023-03-25 23:03:06 +0300

    Fix models for browserbox app

--- browserbox/admin.py
+++ browserbox/admin.py
@@ -1,23 +1,16 @@
 from django.contrib import admin
-from .models import BrowserBox, HistoryBrowserBox, Worker
+from .models import BrowserBox, Worker
 
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
     list_per_page = 20
-    list_display = ('id', 'ip', 'available')
-    search_fields = ('id', 'ip', 'available')
+    list_display = ('id', 'ip', 'status')
+    search_fields = ('id', 'ip', 'status')
 
 
 @admin.register(BrowserBox)
 class BrowserBoxAdmin(admin.ModelAdmin):
-    list_per_page = 20
-    list_display = ('id', 'ip')
-    search_fields = ('id', 'ip')
-
-
-@admin.register(HistoryBrowserBox)
-class BrowserBoxSessionAdmin(admin.ModelAdmin):
     list_per_page = 20
     list_display = ('id', 'ip')
     search_fields = ('id', 'ip')
