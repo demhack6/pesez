9bed50752bb3c97b634e42940d355a8af70d8f2b
devsagul <dev.sagul@gmail.com>
2023-03-25 23:34:52 +0300

    Fix settings module names

--- pesez/asgi.py
+++ pesez/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pfbaas project.
+ASGI config for pesez project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pfbaas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pesez.settings')
 
 application = get_asgi_application()
