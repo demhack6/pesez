bd1cd0a06f1a918b2a622dc1d882d34f0b9dd524
notinfrequently <hard2explain@yandex.ru>
2023-03-25 20:54:02 +0300

    Change browser_box_session to history_broser_box model.

--- browserbox/admin.py
+++ browserbox/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import BrowserBox, BrowserBoxSession
+from .models import BrowserBox, HistoryBrowserBox
 
-admin.site.register(BrowserBox)
-admin.site.register(BrowserBoxSession)
+
+@admin.register(BrowserBox)
+class BrowserBoxAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ('id', 'ip')
+    search_fields = ('id', 'ip')
+
+
+@admin.register(HistoryBrowserBox)
+class BrowserBoxSessionAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ('id', 'ip')
+    search_fields = ('id', 'ip')
