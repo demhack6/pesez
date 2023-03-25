941c29ad219ed7eb966b14e3ce35bd4aa1e0f740
notinfrequently <hard2explain@yandex.ru>
2023-03-25 12:39:14 +0300

    Create user_dashboard app and basic view for authenticated user.

---
+++ user_dashboard/views.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def get_dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'user_dashboard/base.html', {'user': request.user})
+    else:
+        raise PermissionDenied
