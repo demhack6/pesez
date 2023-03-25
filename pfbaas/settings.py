4298602d3596d31b766edf3555aceb2a1e77f197
Ed <33519284+Notinfrequently@users.noreply.github.com>
2023-03-25 11:55:03 +0300

    Create users app with custom user model

--- pfbaas/settings.py
+++ pfbaas/settings.py
@@ -28,9 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
